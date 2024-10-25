ac8f91a913571ed00fcc02b85b2ae9058e1e30a5
İskender <100355107+iskenderErgul@users.noreply.github.com>
2024-10-25 14:00:04 +0300

    User table created

--- resources/js/components/UserManagements/RoleManagement.vue
+++ resources/js/components/UserManagements/RoleManagement.vue
@@ -7,7 +7,7 @@
 
     <div class="flex justify-center">
         <h1>RoleManagment Sayfası</h1>
-        <h1>Tüm kullanıcılar için rol atamaları yeni kullanıcı ekleme silme yetkilendirme vs burda yapılacak bir tablo ile yapılabilir</h1>
+        <h1>Tüm kullanıcılar için rol  saatamaları yeni kullanıcı ekleme silme yetkilendirme vs burda yapılacak bir tablo ile yapılabilir</h1>
 
     </div>
 </template>
