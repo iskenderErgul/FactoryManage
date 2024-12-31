bbaa36ffa31df2ab8a80a450fee3886177c96c65
İskender <100355107+iskenderErgul@users.noreply.github.com>
2024-12-31 15:03:20 +0300

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new RotateShifts)->weeklyOn(1, '00:00');
+        $schedule->job(new RotateShifts)->weeklyOn(0, '00:00');
     }
 
     /**
